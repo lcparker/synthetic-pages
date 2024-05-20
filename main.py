e09318d14f94248565a4b4df8c26df393fd47838
lcp <lachlan.parker03@gmail.com>
2024-05-20 20:03:32 +1000

    added 3d grid creation and cleaned up transform function

--- main.py
+++ main.py
@@ -66,7 +66,29 @@ class Plane:
         self.origin = origin
         self.normal = normal
 
-def deform_plane_as_grad(plane: Plane, bounding_box: BoundingBox2D, grid_density: int):
+
+class HomogeneousTransform:
+    def __init__(self, matrix=None):
+        if matrix is None:
+            self.matrix = np.eye(4)
+        else:
+            assert matrix.shape == (4, 4), "Matrix must be of shape (4, 4)"
+            self.matrix = matrix
+
+    def apply(self, points: np.ndarray) -> np.ndarray:
+        # Ensure points are of shape (..., 3)
+        assert points.shape[-1] == 3, "Points must have shape (..., 3)"
+        
+        # Convert points to homogeneous coordinates by adding a 1 in the last dimension
+        homogeneous_coordinates = np.concatenate([points, np.ones(points.shape[:-1])], axis=-1)
+        transformed_points_homogeneous = homogeneous_coordinates @ self.matrix.T
+
+        # Convert back to 3D coordinates
+        transformed_points = transformed_points_homogeneous[..., :3] / transformed_points_homogeneous[..., 3, np.newaxis]
+        
+        return transformed_points
+
+def deform_plane_as_grad(world_transform: HomogeneousTransform, bounding_box: BoundingBox2D, grid_density: int) -> np.ndarray:
     spline2d = make_2d_spline(bounding_box)
     xs = np.linspace(bounding_box.min[0], bounding_box.max[0], grid_density)
     ys = np.linspace(bounding_box.min[1], bounding_box.max[1], grid_density)
@@ -75,44 +97,43 @@ def deform_plane_as_grad(plane: Plane, bounding_box: BoundingBox2D, grid_density
     y_points = spline2d[1](ys)
 
     X, Y = np.meshgrid(x_points, y_points)
-    plane_grid = np.stack((X.T, Y.T, np.zeros(X.T), axis=-1)
+    plane_grid_3d = np.stack((X.T, Y.T, np.zeros(X.T)), axis=-1)
 
-    # transform the grid from implied coordinates to actual world coords
-    # wait you don't need a plane you need a transform
+    plane_grid_world = world_transform.apply(plane_grid_3d)
 
-    return plane_grid
-
-class HomogeneousTransform:
-                              def __init__(self, matrix=None):
-        def __init__(self, matrix=None):
-        if matrix is None:
-            self.matrix = np.eye(4)
-        else:
-            assert matrix.shape == (4, 4), "Matrix must be of shape (4, 4)"
-            self.matrix = matrix
+    return plane_grid_world.reshape(-1, 3)
     
-# turn plane into mesh
-
-# apply spline deformation to the mesh
-
-
-
-
-
-
-# position mesh in space
-
 # compute distances to mesh
 
+# NOW we want to compute the distance from the spline to the grid to get the
+
+def bounding_box_to_plane(bbox: BoundingBox2D, world_transform: HomogeneousTransform) -> Mesh:
+    """
+    Creates a Trimesh object representing the bounding box as a plane with world_transform applied to it.
+    """
+
+class BoundingBox3D:
+    def __init__(self, min: tuple[float, float, float], max: tuple[float, float, float]):
+        self.min = min
+        self.max = max
+
+def make_grid(bbox: BoundingBox3D, points_per_axis: int) -> np.ndarray:
+    xs = np.linspace(bbox.min[0], bbox.max[0], points_per_axis)
+    ys = np.linspace(bbox.min[1], bbox.max[1], points_per_axis)
+    zs = np.linspace(bbox.min[2], bbox.max[2], points_per_axis)
+
+    X,Y,Z = np.meshgrid(xs, ys, zs)
+
+    grid = np.stack((X.T,Y.T,Z.T), axis=-1)
+    return grid
+
+
+def distance_to_sheet(grid: np.ndarray, sheet: np.ndarray) -> np.ndarray:
+    raise NotImplementedError()
+
+
 """
-but is that really what we want? i don't think so
-
-we want to define a 2d sheet and then get the set of distances from it to form a layer.
-
-
-
-we should be able to do something like
-* generate a plane mesh
-* deform the plane mesh to be a spline
-* for the grid compute distance to the surface
+How to generalise to multiple planes such that they don't intersect?
+* make spline global
+* then apply local deformations as a kernel if necessary
 """
