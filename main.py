e46fbac6c056b163d8330f1dd9d91102f8234840
lcp <lachlan.parker03@gmail.com>
2024-05-20 17:38:27 +1000

    generate 1d spline

--- main.py
+++ main.py
@@ -24,6 +24,30 @@ identity most places except where there are local variations)
 """
 
 import numpy as np
+from scipy.interpolate import make_interp_spline
+
+def make_spline():
+    xs = np.linspace(0,1,10)
+    control_points = np.random.rand(len(xs))* 4 + 5
+    spline = make_interp_spline(xs, control_points) # cubic b-spline fit to control points
+    return spline
 
 
-np.zeros((256,256,256))
+# make 2d spline
+
+# position mesh in space
+
+# compute distances to mesh
+
+"""
+but is that really what we want? i don't think so
+
+we want to define a 2d sheet and then get the set of distances from it to form a layer.
+
+
+
+we should be able to do something like
+* generate a plane mesh
+* deform the plane mesh to be a spline
+* for the grid compute distance to the surface
+"""
