9432e3b868a03dc44e291e36aed8738f49128b32
lcp <lachlan.parker03@gmail.com>
2024-05-31 17:22:41 +1000

    remove old stuff and fix typing for HomogeneousTransform.apply

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
 from scipy.spatial.transform import Rotation
+from typing import Union, TypeVar
 
-Point2D = tuple[float, float]
+type Point2D = tuple[float, float]
 class BoundingBox2D:
     def __init__(self, min: Point2D, max: Point2D):
         self.min = min
@@ -43,25 +44,13 @@ class BoundingBox3D:
 def bernstein(index: int, degree: int, t: np.ndarray) -> np.ndarray:
     return comb(degree, index) * np.power(t, index) * np.power(1 - t, degree - index)
 
-def bezier(control_points: np.ndarray, p: np.ndarray) -> np.ndarray:
-    """
-    Computes Q(u, v) = sum_over(i,j) B_i,j J_n,i(u) K_m,j(v)
+def __test_bernstein_visual():
+    xs = np.linspace(0,1,100)
+    for i in range(5):
+        ys = [bernstein(i, 4 ,x) for x in xs]
+        plt.plot(xs, ys)
 
-    Where B_ij is the ijth control point (scalar) you can adjust the magnitude
-    of the control point value, which is like moving it along the z axis.
-    """ 
-    assert p.shape[-1] == 2
-    assert len(p.shape) == 3 # (H,W,2)
-    n, m = control_points.shape
-    u = p[..., 0]
-    v = p[..., 1]
-    
-    B_u = np.array([bernstein(i, n - 1, u) for i in range(n)])
-    B_v = np.array([bernstein(j, m - 1, v) for j in range(m)])
-    
-    B_uv = np.einsum('ijk, ljk -> iljk', B_u, B_v)
-    zs = np.einsum('ij, ijkl -> kl',control_points, B_uv)
-    return zs[..., None] # (H, W, 1)
+    plt.show()
 
 def bezier_3d(control_points: np.ndarray, p: np.ndarray) -> np.ndarray:
     """
@@ -83,26 +72,6 @@ def bezier_3d(control_points: np.ndarray, p: np.ndarray) -> np.ndarray:
     B_uvw = np.einsum('iN, jN -> Nij', B_u, B_v)
     pts = np.einsum('ija, Nij -> Na',control_points, B_uvw)
     return pts # (N, 3)
-
-def plane_pointcloud_3d_coords(bounding_box: BoundingBox2D, num_points_per_axis: int = 10):
-    """
-    turn bounding box into z=0 grid of bounds uniformly spaced inside
-    bounding_box, with grid_density points per axis
-    """
-    xs = np.linspace(bounding_box.min[0], bounding_box.max[0], num_points_per_axis)
-    ys = np.linspace(bounding_box.min[1], bounding_box.max[1], num_points_per_axis)
-    X, Y = np.meshgrid(xs, ys)
-    Z = np.zeros(X.shape)
-    plane_grid = np.stack((X.T, Y.T, Z.T), axis=-1)
-    return plane_grid
-
-def _test_bernstein_visual():
-    xs = np.linspace(0,1,100)
-    for i in range(5):
-        ys = [bernstein(i, 4 ,x) for x in xs]
-        plt.plot(xs, ys)
-
-    plt.show()
 
 class Mesh:
     def __init__(self, points, triangles):
@@ -165,6 +134,46 @@ class Mesh:
 
         plt.show()
 
+def triangulate_pointcloud(pointcloud: np.ndarray):
+    assert pointcloud.shape[-1] == 3
+    from scipy.spatial import Delaunay
+    triangles = Delaunay(pointcloud[..., :2].reshape(-1, 2)).simplices
+    return Mesh(pointcloud, triangles)
+
+def plane_pointcloud_3d_coords(bounding_box: BoundingBox2D, num_points_per_axis: int = 10):
+    """
+    turn bounding box into z=0 grid of bounds uniformly spaced inside
+    bounding_box, with grid_density points per axis
+    """
+    xs = np.linspace(bounding_box.min[0], bounding_box.max[0], num_points_per_axis)
+    ys = np.linspace(bounding_box.min[1], bounding_box.max[1], num_points_per_axis)
+    X, Y = np.meshgrid(xs, ys)
+    Z = np.zeros(X.shape)
+    plane_grid = np.stack((X.T, Y.T, Z.T), axis=-1)
+    return plane_grid
+
+def unit_plane_3d(num_points_per_axis):
+    unit_bbox = BoundingBox2D((0,0), (1,1))
+    return plane_pointcloud_3d_coords(unit_bbox, num_points_per_axis).reshape(-1, 3)
+
+def bezier_surface(
+        control_points, # (H,W,3)
+        num_points_per_axis: int
+        ) -> Mesh:
+    assert control_points.shape[-1] == 3
+    assert len(control_points.shape) == 3
+
+    pc = unit_plane_3d(num_points_per_axis)
+    pc_3d = bezier_3d(control_points, pc)
+    mesh = triangulate_pointcloud(pc_3d)
+    return mesh
+
+def _test_bezier_visual(control_points, bbox: BoundingBox2D):
+    mesh = bezier_surface(control_points, 10)
+    _, ax = mesh.scene_with_mesh_in_it()
+    ax.scatter(control_points[..., 0], control_points[..., 1], control_points[..., 2])
+    plt.show()
+
 def make_control_points_3d(nx: int, ny: int, bounding_box: BoundingBox2D):
     xs = np.linspace(bounding_box.min[0], bounding_box.max[0], nx)
     ys = np.linspace(bounding_box.min[1], bounding_box.max[1], ny)
@@ -173,11 +182,6 @@ def make_control_points_3d(nx: int, ny: int, bounding_box: BoundingBox2D):
     control_points_3d = np.stack((X.T, Y.T, Z.T), axis=-1)
     return control_points_3d
 
-def triangulate_points(pointcloud: np.ndarray):
-    assert pointcloud.shape[-1] == 3
-    from scipy.spatial import Delaunay
-    triangles = Delaunay(pointcloud[..., :2].reshape(-1, 2)).simplices
-    return Mesh(pointcloud, triangles)
 
 ### MAKING THE VOLUME
 
@@ -200,10 +204,6 @@ def compute_signed_distances(
     sdf = distances.reshape(grid.shape[:-1])
     return sdf
 
-def create_mask(sdf, distance_threshold):
-    mask = sdf <= distance_threshold
-    return mask.astype(int)
-
 import nibabel as nib
 def save_mask_as_nifti(mask, filename):
     # Create a NIfTI image
@@ -217,34 +217,7 @@ def mask_to_mesh(mask) -> Mesh:
     verts, faces, _, _ = marching_cubes(mask, level=0)
     return Mesh(verts, faces)
 
-def mesh_to_3d_page(mesh: Mesh, bbox: BoundingBox3D, distance=.05) -> np.ndarray:
-    grid = make_grid(bbox3d, 100)
-    sdf = compute_signed_distances(grid, mesh)
-    mask = create_mask(sdf, .05)
-    return mask
-
-def unit_plane_3d(num_points_per_axis):
-    unit_bbox = BoundingBox2D((0,0), (1,1))
-    return plane_pointcloud_3d_coords(unit_bbox, num_points_per_axis).reshape(-1, 3)
-
-def bezier_surface(
-        control_points, # (H,W,3)
-        num_points_per_axis: int
-        ) -> Mesh:
-    assert control_points.shape[-1] == 3
-    assert len(control_points.shape) == 3
-
-    pc = unit_plane_3d(num_points_per_axis)
-    pc_3d = bezier_3d(control_points, pc)
-    mesh = triangulate_points(pc_3d)
-    return mesh
-
-def _test_bezier_visual(control_points, bbox: BoundingBox2D):
-    mesh = bezier_surface(control_points, bbox)
-    _, ax = mesh.scene_with_mesh_in_it()
-    ax.scatter(control_points[..., 0], control_points[..., 1], control_points[..., 2])
-    plt.show()
-
+Transformable = TypeVar('Transformable', Mesh, np.ndarray)
 class HomogeneousTransform:
     def __init__(self, matrix=None):
         if matrix is None:
@@ -253,7 +226,7 @@ class HomogeneousTransform:
             assert matrix.shape == (4, 4), "Matrix must be of shape (4, 4)"
             self.matrix = matrix
 
-    def apply(self, x: Mesh | np.ndarray) -> Mesh | np.ndarray:
+    def apply(self, x: Transformable) -> Transformable:
         if isinstance(x, Mesh):
             return self._apply_mesh(x)
         elif isinstance(x, np.ndarray):
@@ -282,6 +255,7 @@ class HomogeneousTransform:
 
 import unittest
 from numpy.testing import assert_array_almost_equal
+
 
 class TestHomogeneousTransform(unittest.TestCase):
     
@@ -340,7 +314,7 @@ def run_transform_tests():
 run_transform_tests()
 
 ###############################
-def generate_random_transform(bbox: BoundingBox3D) -> HomogeneousTransform:
+def random_transform(bbox: BoundingBox3D) -> HomogeneousTransform:
     # Generate random rotation
     rotation = Rotation.random().as_matrix()
     
@@ -362,9 +336,9 @@ def generate_random_transform(bbox: BoundingBox3D) -> HomogeneousTransform:
     
     return HomogeneousTransform(transform_matrix)
 
-def _test_transform_page(control_points, bbox: BoundingBox2D, bbox_3d: BoundingBox3D):
-    mesh = bezier_surface(control_points, bbox)
-    tf = generate_random_transform(bbox_3d)
+def _test_transform_page(control_points, bbox_3d: BoundingBox3D):
+    mesh = bezier_surface(control_points, num_points_per_axis=10)
+    tf = random_transform(bbox_3d)
     mesh = tf.apply(mesh)
 
     _, ax = mesh.scene_with_mesh_in_it()
@@ -410,17 +384,20 @@ def plot_bounding_box(ax, bbox: BoundingBox3D):
     return ax
 
 def _test_bounding_box_vis(control_points, bbox_3d: BoundingBox3D):
-    mesh = bezier_surface(control_points)
+    mesh = bezier_surface(control_points, 10)
     # centre mesh at [0,0,0]
     mesh_centre = mesh.points.mean(axis=0)
     mesh.points = mesh.points - mesh_centre
-    tf = generate_random_transform(bbox_3d)
+    tf = random_transform(bbox_3d)
 
     mesh = tf.apply(mesh)
 
     _, ax = mesh.scene_with_mesh_in_it()
     ax = plot_bounding_box(ax, bbox_3d)
     plt.show()
+
+
+### MULTI PAGE STUFF
 
 def translation(x,y,z) -> HomogeneousTransform:
     # Generate random translation that keeps the object within the bounding box
@@ -436,23 +413,12 @@ def tesselate_pages(control_points, bbox_3d, num_pages, spacing) -> list[Mesh]:
     mesh = Mesh(mesh.points - mesh_centre, mesh.triangles)
     zs = np.linspace(-spacing * num_pages/2, spacing * num_pages/2, num_pages)
     meshes= [translation(0,0,z).apply(mesh) for z in zs]
-    tf = generate_random_transform(bbox_3d)
+    tf = random_transform(bbox_3d)
     meshes= [tf.apply(mesh) for mesh in meshes]
     return meshes
 
-
-
-"""
-SDFs for multiple pages:
-    * compute the sdfs for each mesh individually
-    * collate to (H,W,D,N) array, N being the number of pages
-    * reduce along N, outputting the index that is smallest or NoIndex if it's above the distance threshold
-    * that's the ground truth segmentation data
-"""
-
 def page_meshes_to_volume(page_meshes: list[Mesh], page_thickness: float, bbox_3d: BoundingBox3D):
     grid = make_grid(bbox_3d, 100)
-    # TODO make this more efficient
     sdfs = np.array([compute_signed_distances(grid, m) for m in page_meshes]).transpose((1,2,3,0)) # (H, W, D, N)
     page_labels = np.argmin(sdfs, axis=-1) + 1 # page labels are positive indices
     page_labels[sdfs.min(axis=-1) > page_thickness / 2] = 0 # no page is the zero index
@@ -485,6 +451,7 @@ for m in meshes[1:]:
     fig, ax = m.scene_with_mesh_in_it(fig, ax)
 ax = plot_bounding_box(ax, volume_bbox)
 plt.show()
+
 """
 How to generalise to multiple planes such that they don't intersect?
 * make spline global -> won't work
