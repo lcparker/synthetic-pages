eac9705f02b284146ed53d083f99dcdcc1d7db36
lcp <lachlan.parker03@gmail.com>
2024-11-03 15:28:42 +1000

    use the softmax unet

--- stitch.py
+++ stitch.py
@@ -4,15 +4,20 @@ from pathlib import Path
 
 logging.basicConfig(level=logging.INFO)
 
+
 def get_device():
     from torch import device
     from torch.cuda import is_available as cuda_is_available
-    device = device('cuda' if cuda_is_available() else 'cpu')
+
+    device = device("cuda" if cuda_is_available() else "cpu")
+
 
 def make_network(weights_path: str | Path | None = None) -> UNet:
     import torch
     from torch import nn
+
     device = get_device()
+
     network = UNet(
         spatial_dims=3,
         in_channels=1,
@@ -22,31 +27,63 @@ def make_network(weights_path: str | Path | None = None) -> UNet:
         kernel_size=(7, 3, 3, 3, 3),
         output_bottleneck=True,
         dropout=0.1,
-        # num_res_units=2
     )
+
+    network.to(device)
+
     new_output_conv = nn.Sequential(
-            nn.ConvTranspose3d(128, 64, kernel_size=(3,3,3), stride=(2, 2, 2), padding=(1, 1, 1), output_padding=(1, 1, 1)),
-            nn.ConvTranspose3d(64, 16, kernel_size=(3,3,3), stride=(2, 2, 2), padding=(1, 1, 1), output_padding=(1, 1, 1)),
-            nn.ConvTranspose3d(16, 16, kernel_size=(3,3,3), stride=(1, 1, 1), padding=(1, 1, 1), output_padding=(0, 0, 0)))
-    network.model[2].conv = new_output_conv.to(device)
+        nn.ConvTranspose3d(
+            128,
+            64,
+            kernel_size=(3, 3, 3),
+            stride=(2, 2, 2),
+            padding=(1, 1, 1),
+            output_padding=(1, 1, 1),
+        ),
+        nn.ConvTranspose3d(
+            64,
+            20,
+            kernel_size=(3, 3, 3),
+            stride=(2, 2, 2),
+            padding=(1, 1, 1),
+            output_padding=(1, 1, 1),
+        ),
+        nn.ConvTranspose3d(
+            20,
+            20,
+            kernel_size=(3, 3, 3),
+            stride=(1, 1, 1),
+            padding=(1, 1, 1),
+            output_padding=(0, 0, 0),
+        ),
+    )
+
+    # NOTE I don't know whether this block does anything since the state dict
+    # is loaded immediately after, but just copying tim's code
+    nn.init.kaiming_uniform_(new_output_conv[0].weight)
+    nn.init.kaiming_uniform_(new_output_conv[1].weight)
+    nn.init.kaiming_uniform_(new_output_conv[2].weight)
+    nn.init.constant_(new_output_conv[0].bias, 0.0)
+    nn.init.constant_(new_output_conv[1].bias, 0.0)
+    nn.init.constant_(new_output_conv[2].bias, 0.0)
+
+    network.model[2].conv = new_output_conv.cuda()  # type: ignore
 
     if weights_path:
         network.load_state_dict(torch.load(weights_path))
     else:
-        from scrolls.weights_init import InitWeights_He # TODO fix import here
+        from scrolls.weights_init import InitWeights_He
+
         network.apply(InitWeights_He())
     return network.to(device)
 
-# load surrounding 8 cubes of a given coordinate
-# need to parse command line, download if not already there (maybe? or just error?)
 
 if __name__ == "__main__":
 
     logging.getLogger(__name__).log(logging.INFO, "Running as script")
     logging.getLogger(__name__).log(logging.INFO, "Making network")
-    
-    network = make_network()
 
+    network = make_network()
 
     logging.getLogger(__name__).log(logging.INFO, f"Network weights: {network}")
 
